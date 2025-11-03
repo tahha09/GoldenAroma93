89ed9fda933afb5b0d134e4bc92da099b0a7d7df
tahha09 <ahmed185taha@gmail.com>
2025-11-03 19:31:32 +0200

    small updates

--- golden-aroma/src/app/components/header/header.ts
+++ golden-aroma/src/app/components/header/header.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+declare var bootstrap: any;
 @Component({
   selector: 'app-header',
   imports: [RouterModule, CommonModule],
@@ -8,5 +9,11 @@ import { CommonModule } from '@angular/common';
   styleUrl: './header.scss',
 })
 export class Header {
-
+ closeNavbar() {
+    const navbarCollapse = document.getElementById('navbarNav');
+    if (navbarCollapse?.classList.contains('show')) {
+      const bsCollapse = new bootstrap.Collapse(navbarCollapse, { toggle: true });
+      bsCollapse.hide();
+    }
+  }
 }
